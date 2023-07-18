3e674cecd6111adfcab4abacbea80cd9185b3079
SPS (Seredkin Sergey Petrovich) <73746813+Sergey-Seredkin@users.noreply.github.com>
2023-07-18 18:28:45 +0300

    clear old

--- src/App.js
+++ src/App.js
@@ -153,20 +153,6 @@ function App() {
       
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React SPS
-        </a>
-      </header> */}
       <div>
       {visibleSelect === true ? (
         <div>
