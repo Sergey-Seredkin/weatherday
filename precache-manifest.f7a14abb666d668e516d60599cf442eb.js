5fe0c24bc735096faccfc789d80508a361b3a52d
SPS (Seredkin Sergey Petrovich) <73746813+Sergey-Seredkin@users.noreply.github.com>
2023-07-19 20:36:09 +0300

    Updates

---
+++ precache-manifest.f7a14abb666d668e516d60599cf442eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e2f728ce864c22a14f3",
+    "url": "/weatherday/static/css/main.2e4b817f.chunk.css"
+  },
+  {
+    "revision": "8e2f728ce864c22a14f3",
+    "url": "/weatherday/static/js/main.8e2f728c.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/weatherday/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "13df146c6aeacb61c1aa",
+    "url": "/weatherday/static/css/2.0e05f08b.chunk.css"
+  },
+  {
+    "revision": "13df146c6aeacb61c1aa",
+    "url": "/weatherday/static/js/2.13df146c.chunk.js"
+  },
+  {
+    "revision": "1c8340af37296821d62d",
+    "url": "/weatherday/static/js/runtime~main.1c8340af.js"
+  },
+  {
+    "revision": "073cac35b8f57af1f0779ea8f52e4128",
+    "url": "/weatherday/index.html"
+  }
+];
